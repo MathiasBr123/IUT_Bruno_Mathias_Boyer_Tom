74ba36fe4ae5de79a54eed678d1b1c4547750b24
iutgeiitoulonE105 <touloniut@gmail.com>
2026-09-15 12:31:11 +0200

    booom

--- embedded/robot_boyer_bruno.X/main.c
+++ embedded/robot_boyer_bruno.X/main.c
@@ -7,6 +7,10 @@
 #include "PWM.h"
 #include "ADC.h"
 
+    unsigned int ADCValue0;
+    unsigned int ADCValue1;
+    unsigned int ADCValue2;
+    
 int main(void) {
 
     InitOscillator();
@@ -15,30 +19,32 @@ int main(void) {
     InitTimer1();
     InitPWM();
     InitADC1();
-    
+
     //Le PWM est set a 20%
     //Pour 70% TP_M1_PMWH : 14.30 us +Width
     //Pour 70% TP_M1_PMWL : 413.2 ns +Width
-   PWMSetSpeedConsigne(0, MOTOR_GAUCHE);
-   PWMSetSpeedConsigne(0, MOTOR_DROIT);
-    
+    PWMSetSpeedConsigne(0, MOTOR_GAUCHE);
+    PWMSetSpeedConsigne(0, MOTOR_DROIT);
+
     LED_BLANCHE_1 = 1;
     LED_BLEUE_1 = 1;
     LED_ORANGE_1 = 1;
     LED_ROUGE_1 = 1;
     LED_VERTE_1 = 1;
-    
+
     LED_BLANCHE_2 = 1;
     LED_BLEUE_2 = 1;
     LED_ORANGE_2 = 1;
     LED_ROUGE_2 = 1;
     LED_VERTE_2 = 1;
     
-    while(1){
-        if(ADCConversionFinishedFlag){
+    while (1) {
+        if (ADCConversionFinishedFlag) {
             ADCClearConversionFinishedFlag();
             unsigned int * result = ADCGetResult();
-
+            ADCValue0 = result[0];
+            ADCValue1 = result[1];
+            ADCValue2 = result[2];
         }
     }
 }
